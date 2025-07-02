92bac4caaa7430918fe354c5d952ac37498a6441
josemonteiroCaveo <jose.monteiro@caveo.com.br>
2025-07-02 14:36:11 -0300

    chore:renomeia arquivos e remove identificadores não utilizados

--- app/ui.js
+++ app/ui.js
@@ -18,7 +18,7 @@ import Keyboard from "../core/input/keyboard.js";
 import RFB from "../core/rfb.js";
 import * as WebUtil from "./webutil.js";
 
-const PAGE_TITLE = "noVNC";
+const PAGE_TITLE = "Caveo certificados";
 
 const LINGUAS = ["cs", "de", "el", "es", "fr", "it", "ja", "ko", "nl", "pl", "pt_BR", "ru", "sv", "tr", "zh_CN", "zh_TW"];
 
